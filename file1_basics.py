0933c0bddfffa06e4a180334cf955fec7c8d20cf
gaurav652002 <warrior.prince652002@gmail.com>
2021-10-25 22:47:56 +0530

    File 1 displaying a lable on a window.

---
+++ file1_basics.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+#  Creating a lable widget
+
+label1 = Label(root, text="HEy there welcome to tkinter tutorial, my name is SIlver and im going to guide you through this")
+
+# making it discpkay to the screen
+
+label1.pack()
+
+root.mainloop()
